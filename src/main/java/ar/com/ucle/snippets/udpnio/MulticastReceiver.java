dad98d10814186a3cbb1424f5205faab645ea896
Nadia <nadiasantillan@gmail.com>
2015-02-20 08:22:51 -0300

    Initial commit

---
+++ src/main/java/ar/com/ucle/snippets/udpnio/MulticastReceiver.java
@@ -0,0 +1,139 @@
+package ar.com.ucle.snippets.udpnio;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.StandardProtocolFamily;
+import java.net.StandardSocketOptions;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MulticastReceiver implements Runnable {
+    static class MulticastEndpoint {
+        private String nic;
+        private String address;
+        private int port;
+        Consumer<ByteBuffer> consumer;
+
+        MulticastEndpoint(String nic, String address, int port, Consumer<ByteBuffer> consumer) {
+            this.nic = nic;
+            this.address = address;
+            this.port = port;
+            this.consumer = consumer;
+        }
+    }
+
+    static class ChannelSettings {
+        private DatagramChannel channel;
+        private Consumer<ByteBuffer> consumer;
+
+        ChannelSettings(DatagramChannel channel, Consumer<ByteBuffer> consumer) {
+            this.channel = channel;
+            this.consumer = consumer;
+        }
+
+        public Consumer<ByteBuffer> getConsumer() {
+            return consumer;
+        }
+
+        public DatagramChannel getChannel() {
+            return channel;
+        }
+    }
+
+    private final List<MulticastEndpoint> endpoints;
+    private volatile boolean active;
+
+    private MulticastReceiver(List<MulticastEndpoint> endpoints) {
+        this.endpoints = endpoints;
+        active = true;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public void stop() {
+        active = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Selector selector = createSelector();
+            for (MulticastEndpoint endpoint : endpoints) {
+                createChannel(selector, endpoint);
+            }
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while (active) {
+                Iterator<SelectionKey> keys = getSelectionKeys(selector);
+                while (keys.hasNext()) {
+                    SelectionKey key = keys.next();
+                    ChannelSettings settings = (ChannelSettings) key.attachment();
+                    buffer.clear();
+                    settings.getChannel().receive(buffer);
+                    buffer.flip();
+                    settings.getConsumer().accept(buffer);
+                    keys.remove();
+                }
+
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Error while reading sockets", e);
+        }
+    }
+
+    Iterator<SelectionKey> getSelectionKeys(Selector selector) throws IOException {
+        int ready = selector.select();
+        while (ready == 0) {
+            ready = selector.select();
+        }
+        return selector.selectedKeys().iterator();
+    }
+
+    Selector createSelector() throws IOException {
+        return Selector.open();
+    }
+
+    ChannelSettings createChannel(Selector selector, MulticastEndpoint endpoint) throws IOException {
+        NetworkInterface interf = NetworkInterface.getByName(endpoint.nic);
+        InetAddress group = InetAddress.getByName(endpoint.address);
+
+        DatagramChannel dc = DatagramChannel.open(StandardProtocolFamily.INET)
+                .setOption(StandardSocketOptions.SO_REUSEADDR, true)
+                .bind(new InetSocketAddress(endpoint.port))
+                .setOption(StandardSocketOptions.IP_MULTICAST_IF, interf);
+        dc.configureBlocking(false);
+
+        dc.join(group, interf);
+        ChannelSettings settings = new ChannelSettings(dc, endpoint.consumer);
+        dc.register(selector, SelectionKey.OP_READ, settings);
+        return settings;
+    }
+
+    public static class Builder {
+        private List<MulticastEndpoint> endpoints;
+
+        private Builder() {
+            endpoints = new ArrayList<>(16);
+        }
+
+        public Builder receivesFrom(String groupAddress, int port, String nic, Consumer<ByteBuffer> consumer) {
+            endpoints.add(new MulticastEndpoint(nic, groupAddress, port, consumer));
+
+            return this;
+        }
+
+        public MulticastReceiver build() {
+            return new MulticastReceiver(endpoints);
+        }
+    }
+}
---
+++ src/test/java/ar/com/ucle/snippets/udpnio/MulticastReceiverTest.java
@@ -0,0 +1,86 @@
+package ar.com.ucle.snippets.udpnio;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import static ar.com.ucle.snippets.udpnio.MulticastReceiver.builder;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MulticastReceiverTest {
+    public static final String MESSAGE = "I'm a message";
+    @Mock
+    private DatagramChannel datagramChannel;
+    private Selector selector;
+    private volatile String message;
+
+    @Mock
+    private SelectionKey selectionKey;
+
+    private MulticastReceiver receiver;
+
+    private MulticastReceiver.ChannelSettings settings;
+
+    @Before
+    public void setUp() throws IOException {
+        Consumer<ByteBuffer> consumer = (buffer) -> {
+            int lenght = buffer.getInt();
+            byte[] array = new byte[lenght];
+            buffer.get(array);
+            message = new String(array, 0, lenght);
+        };
+        receiver = spy(builder().receivesFrom("226.1.1.1", 4321, "lo", consumer).build());
+        selector = Selector.open();
+
+        doReturn(selector).when(receiver).createSelector();
+
+        settings = new MulticastReceiver.ChannelSettings(datagramChannel, consumer);
+        selectionKey.attach(settings);
+
+        doReturn(settings).when(receiver).createChannel(eq(selector), any(MulticastReceiver.MulticastEndpoint.class));
+        Set<SelectionKey> keys = new HashSet<>();
+        keys.add(selectionKey);
+        doReturn(keys.iterator()).when(receiver).getSelectionKeys(selector);
+    }
+
+    @Test
+    public void receiverShouldInvokeConsumer() throws InterruptedException, IOException {
+        when(datagramChannel.receive(any(ByteBuffer.class))).thenAnswer(new Answer<SocketAddress>() {
+            @Override
+            public SocketAddress answer(InvocationOnMock invocation) throws Throwable {
+                ByteBuffer buffer = (ByteBuffer) invocation.getArguments()[0];
+                buffer.putInt(MESSAGE.length());
+                buffer.put(MESSAGE.getBytes());
+                return null;
+            }
+        });
+        Thread thread = new Thread(receiver);
+        thread.start();
+
+        Thread.sleep(100);
+        receiver.stop();
+
+        assertEquals(MESSAGE, message);
+        thread.join();
+    }
+}
