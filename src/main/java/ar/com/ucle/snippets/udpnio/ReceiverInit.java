b741610af63c6e2a969699b1f59634ecf6dd5194
Damian Fanaro <Damian.Fanaro@cmegroup.com>
2015-02-23 14:12:46 -0300

    Detailed network information added on MulticastReceiver startup

--- src/main/java/ar/com/ucle/snippets/udpnio/ReceiverInit.java
+++ src/main/java/ar/com/ucle/snippets/udpnio/ReceiverInit.java
@@ -1,10 +1,24 @@
 package ar.com.ucle.snippets.udpnio;
 
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
 public class ReceiverInit {
 
     public static void main(String [] args) {
 
         try {
+
+            /**
+             * Network interfaces listing
+             */
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while(networkInterfaces.hasMoreElements()) {
+                NetworkInterface anInterface = networkInterfaces.nextElement();
+                if(anInterface.isUp()) {
+                    System.out.println("name=" + anInterface.getName() + " display=" + anInterface.getDisplayName() + " multicast=" + anInterface.supportsMulticast());
+                }
+            }
 
             /**
              * Multicast receiver initialization
@@ -15,6 +29,8 @@ public class ReceiverInit {
                     .build();
 
             new Thread(receiver).start();
+
+            System.out.println("Server initialized!");
 
         } catch (Exception e) {
             System.out.println("Invalid arguments. Try [226.1.1.1, 4321, 224.1.1.1, 4322]");
